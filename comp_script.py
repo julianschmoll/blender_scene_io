76503f45bafaa2b0dfbd6e9423a7aec31c762f4e
PhongShaderFrog <115506691+NoriwasabiBoi@users.noreply.github.com>
2024-05-13 14:04:21 +0200

    more output file slots
    found a way to do it

--- comp_script.py
+++ comp_script.py
@@ -9,16 +9,19 @@
     # add file output node
     output_node = nodetree.nodes.new("CompositorNodeOutputFile")
     
-    # output_node.output_file_add_socket()
-    #ä output_file_node.new(Alpha)
+    # add slots ro node
+    output_node.layer_slots.new("Cryptomatte")
     
     # place nodes in comp space
     cryptomatte_node.location = (400,200)
     output_node.location = (1000,100)
     render_layer_node.location = (0,200)
+    # ---------------------------------------------------------------------------- connecting nodes
+    
+    # render layer to output
+    nodetree.links.new(render_layer_node.outputs["Image"],output_node.inputs["Image"])
+    # crypto to output
+    nodetree.links.new(cryptomatte_node.outputs["Matte"],output_node.inputs["Cryptomatte"])
 
-    # connecting nodes
-    nodetree.links.new(cryptomatte_node.outputs["Image"],output_node.inputs[0])
-
-    # nodetree.links.new(render_layer_node.outputs["Image"],
-    nodetree.links.new(render_layer_node.outputs["CryptoObject00"],cryptomatte_node.inputs[0])
+    # render layer to crypto
+    nodetree.links.new(render_layer_node.outputs["Image"],cryptomatte_node.inputs[0])
