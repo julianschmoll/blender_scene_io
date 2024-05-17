b46b897c979e8c9db3f44be39dd909172384cb46
PhongShaderFrog <robmuc@gmx.de>
2024-05-17 11:08:48 +0200

    missing assignment to object

--- material_assigner.py
+++ material_assigner.py
@@ -1,4 +1,5 @@
 import bpy
+import os
 
 
 # texture frodo
@@ -6,27 +7,62 @@ def texture_frodo():
     
     fart = "Fart"
     
+    # create frodo material
     bpy.data.materials.new(fart)
+    
+    # assign to varibale so we can use it
     fart_material = bpy.data.materials.get(fart)
+    
+    # add to frodo
+    frodo = bpy.context.active_object
+    frodo.data.materials.append(fart_material)
+    
+    # enable use nodes to make editable
     fart_material.use_nodes = True
+    
+    # remove default nodes and their connections
     fart_material.node_tree.links.clear()
     fart_material.node_tree.nodes.clear()
-    # create nodes
+    
+    # assign connection and node tree
     nodes = fart_material.node_tree.nodes
     links = fart_material.node_tree.links
-    output = nodes.new(type='ShaderNodeOutputMaterial')
-    image = nodes.new(type='ShaderNodeTexImage')
+    
+    # create nodes
+    output_node = nodes.new(type='ShaderNodeOutputMaterial')
+    texture_node = nodes.new(type='ShaderNodeTexImage')
+    
     # place nodes
-    image.location = (0,200)
-    output.location = (400,200)
+    texture_node.location = (0,200)
+    output_node.location = (400,200)
+    
     # connect nodes
-    links.new(image.outputs['Color'], output.inputs['Surface'])
-    # set path and select color space
-    image_path = ""
-    texture_path = bpy.data.images.load(image_path)
-    texture_path.image = texture_path
+    links.new(texture_node.outputs['Color'], output_node.inputs['Surface'])
     
+    # set path
+    texture_path = "M:/frogging_hell_prism/02_Library/Assets/Character/chr-frodo/Export/Texturing/v007/frodo_skin/chr-frodo_Modeling_v0051_frodo_skin_BaseColor_ACES - ACEScg.1001.png"
     
+    # if texture exists
+    if os.path.exists(texture_path):   
+        
+        # assign loaded image to variable
+        image = bpy.data.images.load(texture_path)
+        
+        # create texture and assign to variable
+        texture = bpy.data.textures.new(name="Fart", type='IMAGE')
+        
+        # assign loaded image to image texture
+        texture.image = image
+        texture_node.image = image
+
+        # set texture mapping scale
+        texture_node.texture_mapping.scale[0] = 1.0
+        texture_node.texture_mapping.scale[1] = 1.0
+        
+        # set color space
+        bpy.data.images["chr-frodo_Modeling_v0051_frodo_skin_BaseColor_ACES - ACEScg.004"].colorspace_settings.name = 'ACES - ACEScg'
+
+        
     
 # texture bed
 def texture_bed():
