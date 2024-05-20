44044c45bb1a512f20048c7ab97f566112e0e305
js435 <dev@julianschmoll.de>
2024-05-20 16:52:18 +0200

    only run register menu

--- startup.py
+++ startup.py
@@ -1,7 +1,6 @@
 from blender_scene_io import comp_script
-from blender_scene_io import material_assigner
 from blender_scene_io import render_setup
-from blender_scene_io import sort_outliner
+from blender_scene_io import frogging_hell_menu
 
 import logging
 
@@ -11,11 +10,4 @@ LOGGER = logging.getLogger("blender_scene_io")
 def run_startup_scripts():
     """This method is always run when launching blender."""
     LOGGER.info("Running Startup Scripts...")
-    sort_outliner.sort_scene()
-    render_setup.main()
-    assign_materials()
-    comp_script.basic_comp_setup()
-
-def assign_materials():
-    material_assigner.texture_frodo()
-    material_assigner.texture_bed()
+    frogging_hell_menu.register()
