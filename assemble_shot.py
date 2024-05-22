5a2ab099d1a27a3d67cb5defa246b2eaa336ba27
PhongShaderFrog <robmuc@gmx.de>
2024-05-22 16:32:27 +0200

    can create collections, load cache and link to collection, still some errors though

--- assemble_shot.py
+++ assemble_shot.py
@@ -5,16 +5,66 @@ import os
 LOGGER = logging.getLogger("Frogging Hell Menu")
 
 def load_shot(shot_caches):
+
+    import_group = []
     # go through caches and load them
     for cache in shot_caches:
-        # cache_collection =
+        # log cache import
         LOGGER.info(f"Loading {cache}")
-        cache_name = os.path.basename(cache)
-        cache_name = cache_name[13:-4]
-        bpy.ops.collection.create(name = cache_name)
-        LOGGER.info(f"Collection: {cache_name}")
-        bpy.context.scene.collection.children.link(bpy.data.collections[cache_name])
-        bpy.ops.wm.alembic_import(filepath=cache)
+        cache_name= split_cache(cache)
+        cache_collection = create_collection(cache_name)
+        imported_objects =  import_alembic(cache)
+        root_object = get_imported_root_objects(imported_objects)
+        link_to_collection(root_object,cache_name)
+        LOGGER.info(f"Loaded and sorted {cache} in collection")
 
-caches = ['M:\\frogging_hell_prism\\02_Library\\Shots\\500-010\\Export\\Animation\\v0011_arms-fixxed_lre\\centimeter\\shot_500-010_chr-frodo-rig.abc', 'M:\\frogging_hell_prism\\02_Library\\Shots\\500-010\\Export\\Animation\\v0011_arms-fixxed_lre\\centimeter\\shot_500-010_prp-bed-rig.abc']
-load_shot(caches)
+    LOGGER.info(f"{import_group}")
+
+# create collection
+def create_collection(cache_name):
+    bpy.data.collections.new(cache_name)
+    bpy.context.scene.collection.children.link(bpy.data.collections[cache_name])
+    return bpy.data.collections[cache_name]
+
+# get root of imported cache
+def get_root(cache):
+    """Get the root of an object hierarchy here"""
+    parent = cache.parent
+    # check if parent exists
+    if parent:
+        return get_root(parent)
+    return cache
+
+# get root of import
+def get_imported_root_objects(imported_objects):
+    """Get the root object of cache"""
+    imported_root = ""
+    for ob in imported_objects:
+        imported_root = get_root(ob)
+    return imported_root
+
+# import alembic cache
+def import_alembic(cache):
+    """Import cache and return names of import."""
+    # Get current objects before import
+    pre_import_objects = set(bpy.context.scene.objects)
+    # Import the cache
+    bpy.ops.wm.alembic_import(filepath=cache)
+    # Get objects after import
+    post_import_objects = set(bpy.context.scene.objects)
+    # Determine the new objects
+    imported_objects = post_import_objects - pre_import_objects
+    return imported_objects
+
+# link root to collection named after import
+def link_to_collection(root_object, collection):
+    """Unlink imported root from other collections and link it to the new created collection"""
+    bpy.context.scene.collection.objects.unlink(root_object)
+    bpy.data.collections[collection].objects.link(root_object)
+
+
+# get cache name from path
+def split_cache(cache):
+    cache_name = os.path.basename(cache)
+    cache_name = cache_name[13:-4]
+    return cache_name
