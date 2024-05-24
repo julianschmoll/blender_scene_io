132e75cbb0af4f61484dc80243cb5650eecaf635
PhongShaderFrog <robmuc@gmx.de>
2024-05-25 01:33:59 +0200

    loops through objects in scene now and if the objects name is in the dictionary creates a new material for it, assigns it and then loads the value image fromm the dictionary.

--- material_assigner.py
+++ material_assigner.py
@@ -1,3 +1,5 @@
+from blender_scene_io import texture_dictionary
+
 import bpy
 import os
 import logging
@@ -6,63 +8,65 @@ logging.basicConfig(level=logging.INFO)
 LOGGER = logging.getLogger("blender_scene_io")
 
 # texture frodo
-def texture_frodo():
-    
-    fart = "Fart"
-    
+def texture_objects_in_scene():
+
+    LOGGER.info("Running Texture Script...")
+
+    texture_dict = texture_dictionary.texture_dict
+    # go through dictionary
+    for obj in bpy.data.objects:
+        # loop through dictionary
+        for key in texture_dict:
+            # check if names are the same
+            if obj.name!=key:
+                # if not go to next key
+                continue
+            else:
+                # check if texture image exists
+                if os.path.exists(texture_dict.value):
+                    texture_material = key.split(":")[-1]
+                    # create new material named after key
+                    create_material(obj,texture_material,texture_dictionary.value)
+
+def create_material(obj,texture_material,path):
+    """
+    This function creates a new image texture material node, links it to an output node.
+    The new material is named after the key in the dictionary (eg. frog_whole_pants).
+    It is thenm assigned to the obj which matches the keys name.
+    The key's value is used as the path to load the image.
+
+    """
     # create frodo material
-    bpy.data.materials.new(fart)
-    
+    bpy.data.materials.new(texture_material)
     # assign to varibale so we can use it
-    fart_material = bpy.data.materials.get(fart)
-    
-    # add to frodo
-    frodo = bpy.context.active_object
-    frodo.data.materials.append(fart_material)
-    
+    new_material = bpy.data.materials.get(texture_material)
+    # add to obj
+    obj.data.materials.append(new_material)
     # enable use nodes to make editable
-    fart_material.use_nodes = True
-    
+    new_material.use_nodes = True
     # remove default nodes and their connections
-    fart_material.node_tree.links.clear()
-    fart_material.node_tree.nodes.clear()
-    
+    new_material.node_tree.links.clear()
+    new_material.node_tree.nodes.clear()
     # assign connection and node tree
-    nodes = fart_material.node_tree.nodes
-    links = fart_material.node_tree.links
-    
+    nodes = new_material.node_tree.nodes
+    links = new_material.node_tree.links
     # create nodes
     output_node = nodes.new(type='ShaderNodeOutputMaterial')
     texture_node = nodes.new(type='ShaderNodeTexImage')
-    
     # place nodes
     texture_node.location = (0,200)
     output_node.location = (400,200)
-    
     # connect nodes
     links.new(texture_node.outputs['Color'], output_node.inputs['Surface'])
-    
-    # set path
-    texture_path = "M:/frogging_hell_prism/02_Library/Assets/Character/chr-frodo/Export/Texturing/v007/frodo_skin/chr-frodo_Modeling_v0051_frodo_skin_BaseColor_ACES - ACEScg.1001.png"
-    
-    # if texture exists
-    if os.path.exists(texture_path):   
-        
-        # assign loaded image to variable
-        image = bpy.data.images.load(texture_path)
-        
-        # create texture and assign to variable
-        texture = bpy.data.textures.new(name="Fart", type='IMAGE')
-        
-        # assign loaded image to image texture
-        texture.image = image
-        texture_node.image = image
-
-        # set texture mapping scale
-        texture_node.texture_mapping.scale[0] = 1.0
-        texture_node.texture_mapping.scale[1] = 1.0
-        
-        # set color space
-        bpy.data.images["chr-frodo_Modeling_v0051_frodo_skin_BaseColor_ACES - ACEScg.004"].colorspace_settings.name = 'ACES - ACEScg'
-
-    LOGGER.info("Running Texture Script...")
+    # assign loaded image to variable
+    image = bpy.data.images.load(path)
+    # create texture and assign to variable
+    texture = bpy.data.textures.new(name=texture_material, type='IMAGE')
+    # assign loaded image to image texture
+    texture.image = image
+    texture_node.image = image
+    # set texture mapping scale
+    texture_node.texture_mapping.scale[0] = 1.0
+    texture_node.texture_mapping.scale[1] = 1.0
+    # set color space
+    bpy.data.images["chr-frodo_Modeling_v0051_frodo_skin_BaseColor_ACES - ACEScg.004"].colorspace_settings.name = 'ACES - ACEScg'
