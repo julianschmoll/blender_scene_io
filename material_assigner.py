8f547352d94f8ae77eb853735df21295bd42a69a
PhongShaderFrog <robmuc@gmx.de>
2024-05-27 23:38:14 +0200

    added grease pencil line art object

--- material_assigner.py
+++ material_assigner.py
@@ -7,28 +7,33 @@ import logging
 logging.basicConfig(level=logging.INFO)
 LOGGER = logging.getLogger("blender_scene_io")
 
-# texture frodo
-def texture_objects_in_scene():
-    LOGGER.info("Running Shader Script...")
-    cel_shade()
-    texture_dict = texture_dictionary.texture_dict
-    matte_dict = texture_dictionary.matte_dict
-    matte_pain("mattes", matte_dict)
-    # go through dictionary
-    for obj in bpy.data.objects:
-        # loop through dictionary
-        for key in texture_dict:
-            # check if names are the same
-            if obj.name!=key:
-                # if not go to next key
-                continue
-            else:
-                # check if texture image exists
-                if os.path.exists(texture_dict[key]):
-                    texture_material = key.split(":")[-1]
-                    # create new material named after key
-                    create_image_texture_material(obj,texture_material,texture_dict[key])
+def texture_path(collection, input_dictionary):
+    """This function finds a texture for every object that has one.
+        The function iterates through all objects in the input collection.
+        If it's a mesh and has a key, this function will use the function create_image_texture_material
+        (it creates a new image texture material for each object that has an entry in a dictionary).
+        input_dictionary is a string that is a key in the big texture_dict.
 
+    """
+    # iterate through input collection
+    for obj in bpy.data.collections[collection.name].objects:
+        # if object is a mesh continue
+        if obj.type == 'MESH':
+            # go through dictionaries in big dictionary
+            for key in texture_dictionary.texture_dict:
+                # check if collection has a key in big dictionary
+                if key == input_dictionary:
+                    for input_key in texture_dictionary.texture_dict[input_dictionary]:
+                        # check if names are the same
+                        if obj.name != input_key:
+                            # if not go to next key
+                            continue
+                        else:
+                            # check if texture image exists
+                            if os.path.exists(texture_dictionary.texture_dict[input_dictionary][input_key]):
+                                texture_material = input_key.split(":")[-1]
+                                # create new material named after key
+                                create_image_texture_material(obj, texture_material, texture_dictionary.texture_dict[input_dictionary][input_key])
 
 def create_image_texture_material(obj,texture_material,path):
     """
@@ -36,7 +41,6 @@ def create_image_texture_material(obj,texture_material,path):
     The new material is named after the key in the dictionary (eg. frog_whole_pants).
     It is then assigned to the obj which matches the keys name.
     The key's value is used as the path to load the image.
-
     """
     # create frodo material
     bpy.data.materials.new(texture_material)
@@ -106,18 +110,44 @@ def cel_shade():
         if obj.type=='MESH':
             obj.data.materials.append(cel_shader)
 
-def matte_pain(collection, input_dictionary):
-    """This function textures the walls with the matte paintings"""
-    for obj in bpy.data.collections[collection].objects:
-        if obj.type == 'MESH':
-            for key in input_dictionary:
-                # check if names are the same
-                if obj.name != key:
-                    # if not go to next key
-                    continue
-                else:
-                    # check if texture image exists
-                    if os.path.exists(input_dictionary[key]):
-                        texture_material = key.split(":")[-1]
-                        # create new material named after key
-                        create_image_texture_material(obj, texture_material, input_dictionary[key])
+def guilded_grease(collection):
+    # create gpencil and name it
+    gpencil_data = bpy.data.grease_pencils.new(name=collection.name)
+    gpencil_object = bpy.data.objects.new(name=collection.name + "_GP", object_data=gpencil_data)
+    # add it to the collection
+    bpy.data.collections[collection.name].objects.link(gpencil_object)
+    # create a new gpencil layer
+    gp_layer = gpencil_data.layers.new(name=collection.name+"_layer", set_active=True)
+    # create a gpencil frame
+    gp_frame = gp_layer.frames.new(0)
+    # create a Line Art modifier for the gpencil object
+    lineart_mod = gpencil_object.grease_pencil_modifiers.new(name="LineArt", type='GP_LINEART')
+    # create modifier layer
+    lineart_mod.target_layer = collection.name+"_layer"
+    # create material for lines
+    gp_mat = bpy.data.materials.new(name=collection.name + "_Black")
+    gpencil_data.materials.append(gp_mat)
+    # select gp material
+    lineart_mod.target_material = gp_mat
+    # don't use crease pls man
+    lineart_mod.use_crease = False
+    # Set the target collection for the Line Art modifier
+    lineart_mod.source_collection = collection
+    # show in viewport
+    lineart_mod.show_render=True
+    lineart_mod.show_viewport=True
+    # make thinner
+    lineart_mod.thickness = 10
+    # subdivide gpencil
+    subdiv_mod = gpencil_object.grease_pencil_modifiers.new(name="Subdivision", type="GP_SUBDIV")
+    subdiv_mod.level=5
+    # Return the created Grease Pencil object
+    return gpencil_object
+
+def slim_shade():
+    LOGGER.info("Running Shader Script...")
+    cel_shade()
+    for collection in bpy.data.collections:
+        if collection.name != "cami":
+            texture_path(collection, collection.name.replace("-","_")+"_dict")
+            guilded_grease(collection)
