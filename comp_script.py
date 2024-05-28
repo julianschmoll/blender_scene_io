cc2b225ad10dd4d41aead23cb8444126bed495f2
PhongShaderFrog <robmuc@gmx.de>
2024-05-28 12:10:26 +0200

    working on seconf cryptomatte node

--- comp_script.py
+++ comp_script.py
@@ -7,11 +7,12 @@ logging.basicConfig(level=logging.INFO)
 LOGGER = logging.getLogger("blender_scene_io")
 
 # -------------------------------------------------------------------------------- COMP ---------------------------------------------------------------------
+def comp_setup():
+    basic_comp_setup()
+
 
 def pick_cryptomatte_materials(material_names, cryptomatte_node):
-    for name in material_names:
-        cryptomatte_node.matte_id = name
-        bpy.ops.compositor.cryptomatte_add()
+    cryptomatte_node.matte_id = ",".join(material_names)
 
 def basic_comp_setup():
     bpy.context.scene.use_nodes = True
@@ -22,20 +23,28 @@ def basic_comp_setup():
     render_layer_node = nodetree.nodes.new("CompositorNodeRLayers")
     # add cryptomatte node
     frodo_cryptomatte_node = nodetree.nodes.new("CompositorNodeCryptomatteV2")
+    # add second cryptomatte node
+    # bed_cryptomatte_node = nodetree.nodes.new("CompositorNodeCryptomatteV2")
     # add file output node
     output_node = nodetree.nodes.new("CompositorNodeOutputFile")
     # add slots ro node
-    output_node.layer_slots.new("Cryptomatte")
+    output_node.layer_slots.new("Cryptomatte_Frodo")
+    output_node.layer_slots.new("Cryptomatte_Bed")
     # place nodes in comp space
-    frodo_cryptomatte_node.location = (400,200)
+    frodo_cryptomatte_node.location = (400,400)
+    #bed_cryptomatte_node.location = (400,-200)
     output_node.location = (1000,100)
     render_layer_node.location = (0,200)
     # ---------------------------------------------------------------------------- connecting nodes
     # render layer to output
     nodetree.links.new(render_layer_node.outputs["Image"],output_node.inputs["Image"])
     # crypto to output
-    nodetree.links.new(frodo_cryptomatte_node.outputs["Matte"],output_node.inputs["Cryptomatte"])
-    # render layer to crypto
+    nodetree.links.new(frodo_cryptomatte_node.outputs["Image"],output_node.inputs["Cryptomatte_Frodo"])
+    #nodetree.links.new(bed_cryptomatte_node.outputs["Matte"]),output_node.inputs["Cryptomatte_Bed"]
+    # render layer to cryptomatte
     nodetree.links.new(render_layer_node.outputs["Image"],frodo_cryptomatte_node.inputs[0])
-    LOGGER.info("Running Comp Script...")
+    nodetree.links.new(render_layer_node.outputs["Image"],bed_cryptomatte_node.inputs[0])
+    frodo_cryptomatte_node.layer_name = 'ViewLayer.CryptoMaterial'
+    #bed_cryptomatte_node.layer_name = 'ViewLayer.CryptoMaterial'
     pick_cryptomatte_materials(texture_dictionary.frodo_list,frodo_cryptomatte_node)
+    #pick_cryptomatte_materials(texture_dictionary.bed_list,bed_cryptomatte_node)
