37d5534d4e61ab401e6a398e0d5013c1469975e5
PhongShaderFrog <robmuc@gmx.de>
2024-05-30 16:51:56 +0200

    turned it into one dict so it's easier in material_assigner.py

--- texture_dictionary.py
+++ texture_dictionary.py
@@ -53,45 +53,41 @@ bed_list = ["prp_mattress",
 ]
 
 texture_dict = {
-    "mattes_dict":{
-        "set_room_Layout:door_plane":os.path.join(
+        "door_plane":os.path.join(
             base_dir,
             matte_path,
             "projection_walls-door_plane.png"
         ),
-        "set_room_Layout:floor_plane_right":os.path.join(
+        "floor_plane_right":os.path.join(
             base_dir,
             matte_path,
             "projection_walls-floor_plane.png"
         ),
-        "set_room_Layout:walls_but_thick_right_seperate"
-        "":os.path.join(
+        "walls_but_thick_right_seperate":os.path.join(
             base_dir,
             matte_path,
             "projection_walls-walls_but_thick_right_seperate.png"
         ),
-        "set_room_Layout:walls_but_thick_right":os.path.join(
+        "walls_but_thick_right":os.path.join(
             base_dir,
             matte_path,
             "projection_walls-walls_but_thick_right.png"
         ),
-        "set_room_Layout:walls_but_thick_left":os.path.join(
+        "walls_but_thick_left":os.path.join(
             base_dir,
             matte_path,
             "projection_walls-walls_but_thick_left.png"
         ),
-        "set_room_Layout:walls_but_thick_left_seperate":os.path.join(
+        "walls_but_thick_left_seperate":os.path.join(
             base_dir,
             matte_path,
             "projection_walls-walls_but_thick_left_seperate.png"
         ),
-        "set_room_Layout:window_plane":os.path.join(
+        "window_plane":os.path.join(
             base_dir,
             matte_path,
             "projection_walls-window_plane.png"
-        )
-    },
-    "chr_frodo_rig_dict":{
+        ),
         # frodo pants
         "chr_frodo_Rigging:chr_frodo_Modeling:frog_whole_pants":os.path.join(
             base_dir,
@@ -137,52 +133,46 @@ texture_dict = {
         # eye lid left
         "chr_frodo_Rigging:eye_l_lower_lid":os.path.join(
             "M:/frogging_hell_prism/06_Artist/DAS_ROB/chr-frodo_skin_BaseColor_ACES - ACEScg.1001.png"),
+        "chr_frodo_Rigging:eye_l_upper_lid": os.path.join(
+            "M:/frogging_hell_prism/06_Artist/DAS_ROB/chr-frodo_skin_BaseColor_ACES - ACEScg.1001.png"),
         # eye lid right
         "chr_frodo_Rigging:eye_r_lower_lid":os.path.join(
             "M:/frogging_hell_prism/06_Artist/DAS_ROB/chr-frodo_skin_BaseColor_ACES - ACEScg.1001.png"),
-        "chr_frodo_Rigging:eye_l_upper_lids":os.path.join(
-            "M:/frogging_hell_prism/06_Artist/DAS_ROB/chr-frodo_skin_BaseColor_ACES - ACEScg.1001.png"),
         "chr_frodo_Rigging:eye_r_upper_lid":os.path.join(
             "M:/frogging_hell_prism/06_Artist/DAS_ROB/chr-frodo_skin_BaseColor_ACES - ACEScg.1001.png"),
-    },
-
-    "prp_bed_rig_dict":{
         # bed frame
-        "set_room_Layout:prp_bed_Rigging:prp_bed_Modeling:bedframe":os.path.join(
+        "prp_bed_Rigging:prp_bed_Modeling:bedframe":os.path.join(
             base_dir,
             get_version(bed_path),
             "bed_frame/prp-bed_Modeling_v0020_bed_frame_mat_BaseColor_ACES - ACEScg.1001.png"
         ),
         # bed pillow
-        "set_room_Layout:prp_pillow_Modeling:fat_pillow_mod":os.path.join(
+        "prp_pillow_Modeling:fat_pillow_mod":os.path.join(
            "M:/frogging_hell_prism/02_Library/Assets/Props/prp - pillow/Export/Texturing/v001"
         ),
         # bed matress
-        "set_room_Layout:prp_bed_Rigging:prp_bed_Modeling:prp_mattress":os.path.join(
+        "prp_bed_Rigging:prp_bed_Modeling:prp_mattress":os.path.join(
             base_dir,
             get_version(bed_path),
             "bed_matress/prp-bed_Modeling_v0020_matress_mat_BaseColor_ACES - ACEScg.1001.png"
         ),
         # bed balls
-        "set_room_Layout:prp_bed_Rigging:prp_bed_Modeling:balls1":os.path.join(
+        "prp_bed_Rigging:prp_bed_Modeling:balls1":os.path.join(
             base_dir,
             get_version(bed_path),
             "bed_frame/prp-bed_Modeling_v0020_bed_frame_mat_BaseColor_ACES - ACEScg.1002.png"),
-        "set_room_Layout:prp_bed_Rigging:prp_bed_Modeling:balls2":os.path.join(
+        "prp_bed_Rigging:prp_bed_Modeling:balls2":os.path.join(
             base_dir,
             get_version(bed_path),
             "bed_frame/prp-bed_Modeling_v0020_bed_frame_mat_BaseColor_ACES - ACEScg.1002.png"),
-        "set_room_Layout:prp_bed_Rigging:prp_bed_Modeling:balls3":os.path.join(
+        "prp_bed_Rigging:prp_bed_Modeling:balls3":os.path.join(
             base_dir,
             get_version(bed_path),
             "bed_frame/prp-bed_Modeling_v0020_bed_frame_mat_BaseColor_ACES - ACEScg.1002.png"),
-        "set_room_Layout:prp_bed_Rigging:prp_bed_Modeling:balls4":os.path.join(
+        "prp_bed_Rigging:prp_bed_Modeling:balls4":os.path.join(
             base_dir,
             get_version(bed_path),
-            "bed_frame/prp-bed_Modeling_v0020_bed_frame_mat_BaseColor_ACES - ACEScg.1002.png")
-    }
-}
-painting_dict = {
+            "bed_frame/prp-bed_Modeling_v0020_bed_frame_mat_BaseColor_ACES - ACEScg.1002.png"),
     # gertie
     #"set_room_Layout:gertie1":os.path.join(
     #        "M:/frogging_hell_prism/06_Artist/random/2DAssets/02_export/gertie.png"),
@@ -191,23 +181,189 @@ painting_dict = {
     #        base_dir,
     #        "Props/prp-standing-frame/Export/Texturing/prp_standing_frame_Modeling_frame.png"),
     # standing frame horizontal
-    "set_room_Layout:prp_standing_frame_Rigging:prp_standing_frame_M":os.path.join(
+    "prp_standing_frame_Rigging:prp_standing_frame_M":os.path.join(
             base_dir,
             "Props/prp-standing-frame/Export/Texturing/prp_standing_frame_Modeling_frame.png"),
-    # urne
-    #"set_room_Layout:prp_urn_flip_Modeling:urn":os.path.join(
-    #        base_dir,
-    #        "Props/prp-urn-flip/Export/Texturing/v001/prp-urn-flip_Modeling_v0003_DefaultMaterial_BaseColor_ACES - ACEScg.1001.png"),
+    # ----------------------------------------------------------------  drawer
+    # drawer body
+    "prp_drawer_Rigging:prp_drawer_Modeling:prp_body":os.path.join(
+            base_dir,
+            "Props/prp-drawer/Export/Texturing/v001/body_mat/prp-drawer_Modeling_v0010_prp_body_mat_BaseColor_ACES - ACEScg.1001.png"),
+    # drawer drawer
+    "prp_drawer_Rigging:prp_drawer_Modeling:prp_fron":os.path.join(
+            base_dir,
+            "Props/prp-drawer/Export/Texturing/v001/drawer_mat/prp-drawer_Modeling_v0010_prp_drawer_mat_BaseColor_ACES - ACEScg.1001.png"),
+    "prp_drawer_Rigging:prp_drawer_Modeling:prp_.002":os.path.join(
+            base_dir,
+            "Props/prp-drawer/Export/Texturing/v001/drawer_mat/prp-drawer_Modeling_v0010_prp_drawer_mat_BaseColor_ACES - ACEScg.1001.png"),
+    # rings
+    "prp_drawer_Rigging:prp_drawer_Modeling:prp_ring":os.path.join(
+            base_dir,
+            "Props/prp-drawer/Export/Texturing/v001/ring_mat/prp-drawer_Modeling_v0010_prp_ring_mat_BaseColor_ACES - ACEScg.1001.png"),
+    "prp_drawer_Rigging:prp_drawer_Modeling:prp_ball":os.path.join(
+            base_dir,
+            "Props/prp-drawer/Export/Texturing/v001/ring_mat/prp-drawer_Modeling_v0010_prp_ring_mat_BaseColor_ACES - ACEScg.1001.png"),
+    "prp_drawer_Rigging:prp_drawer_Modeling:poly.004":os.path.join(
+            base_dir,
+            "Props/prp-drawer/Export/Texturing/v001/ring_mat/prp-drawer_Modeling_v0010_prp_ring_mat_BaseColor_ACES - ACEScg.1001.png"),
+    "prp_drawer_Rigging:prp_drawer_Modeling:poly.001":os.path.join(
+            base_dir,
+            "Props/prp-drawer/Export/Texturing/v001/ring_mat/prp-drawer_Modeling_v0010_prp_ring_mat_BaseColor_ACES - ACEScg.1001.png"),
+    "prp_drawer_Rigging:prp_drawer_Modeling:poly.003":os.path.join(
+            base_dir,
+            "Props/prp-drawer/Export/Texturing/v001/ring_mat/prp-drawer_Modeling_v0010_prp_ring_mat_BaseColor_ACES - ACEScg.1001.png"),
+    "prp_drawer_Rigging:prp_drawer_Modeling:polySurf":os.path.join(
+            base_dir,
+            "Props/prp-drawer/Export/Texturing/v001/ring_mat/prp-drawer_Modeling_v0010_prp_ring_mat_BaseColor_ACES - ACEScg.1001.png"),
+    "prp_drawer_Rigging:prp_drawer_Modeling:poly.002":os.path.join(
+            base_dir,
+            "Props/prp-drawer/Export/Texturing/v001/ring_mat/prp-drawer_Modeling_v0010_prp_ring_mat_BaseColor_ACES - ACEScg.1001.png"),
+    "prp_drawer_Rigging:prp_drawer_Modeling:poly":os.path.join(
+            base_dir,
+            "Props/prp-drawer/Export/Texturing/v001/ring_mat/prp-drawer_Modeling_v0010_prp_ring_mat_BaseColor_ACES - ACEScg.1001.png"),
+    # legs
+    "prp_drawer_Rigging:prp_drawer_Modeling:prp_leg3":os.path.join(
+            base_dir,
+            "Props/prp-drawer/Export/Texturing/v001/leg_mat/prp-drawer_Modeling_v0010_prp_leg_mat_BaseColor_ACES - ACEScg.1001.png"),
+    "prp_drawer_Rigging:prp_drawer_Modeling:prp_leg2":os.path.join(
+            base_dir,
+            "Props/prp-drawer/Export/Texturing/v001/leg_mat/prp-drawer_Modeling_v0010_prp_leg_mat_BaseColor_ACES - ACEScg.1001.png"),
+    "prp_drawer_Rigging:prp_drawer_Modeling:prp_leg1":os.path.join(
+            base_dir,
+            "Props/prp-drawer/Export/Texturing/v001/leg_mat/prp-drawer_Modeling_v0010_prp_leg_mat_BaseColor_ACES - ACEScg.1001.png"),
+    "prp_drawer_Rigging:prp_drawer_Modeling:prp_leg":os.path.join(
+            base_dir,
+            "Props/prp-drawer/Export/Texturing/v001/leg_mat/prp-drawer_Modeling_v0010_prp_leg_mat_BaseColor_ACES - ACEScg.1001.png"),
+    # ------------------------------------------------------------------ urne
+    "prp_urn_flip_Modeling:urn":os.path.join(
+            base_dir,
+            "Props/prp-urn-flip/Export/Texturing/v001/prp-urn-flip_Modeling_v0003_DefaultMaterial_BaseColor_ACES - ACEScg.1001.png"),
+    "prp_urn_flip_Modeling:urntop":os.path.join(
+            base_dir,
+            "Props/prp-urn-flip/Export/Texturing/v001/prp-urn-flip_Modeling_v0003_DefaultMaterial_BaseColor_ACES - ACEScg.1001.png"),
+    # ------------------------------------------------------------------ small paint buckets
+    "prp_paint_bucket_small_Rigging1:prp_paint_b":os.path.join(
+            base_dir,
+            "Props/prp-paint-bucket-small/Export/Texturing/v001/bucket_mat/prp-paint-bucket-small_Modeling_v0004_bucket_mat_BaseColor_ACES - ACEScg.1001.png"),
+    "prp_paint_bucket_small_Rigging1:prp_paint_bucke":os.path.join(
+            base_dir,
+            "Props/prp-paint-bucket-small/Export/Texturing/v001/handle_mat/prp-paint-bucket-small_Modeling_v0004_handle_mat_BaseColor_ACES - ACEScg.1001.png"),
+    "prp_paint_bucket_small_Rigging:prp_paint_bu": os.path.join(
+        base_dir,
+        "Props/prp-paint-bucket-small/Export/Texturing/v001/bucket_mat/prp-paint-bucket-small_Modeling_v0004_bucket_mat_BaseColor_ACES - ACEScg.1001.png"),
+    "prp_paint_bucket_small_Rigging:prp_paint_bucket": os.path.join(
+        base_dir,
+        "Props/prp-paint-bucket-small/Export/Texturing/v001/handle_mat/prp-paint-bucket-small_Modeling_v0004_handle_mat_BaseColor_ACES - ACEScg.1001.png"),
+    # palette
+    "prp_palette_Rigging1:prp_palette_Modeling:palet":os.path.join(
+            base_dir,
+            "Props/prp-palette/Export/Texturing/v001/prp-palette_Modeling_v0005_palette_mat_BaseColor_ACES - ACEScg.1001.png"),
+    "prp_palette_Rigging:prp_palette_Modeling:palett": os.path.join(
+        base_dir,
+        "Props/prp-palette/Export/Texturing/v001/prp-palette_Modeling_v0005_palette_mat_BaseColor_ACES - ACEScg.1001.png"),
+    # ------------------------------------------------------------------ paint bucket
+    "prp_paint_bucket_Rigging2:prp_paint_bucket_Mode": os.path.join(
+        base_dir,
+        "Props/prp-paint-bucket/Export/Texturing/v001/bucket_mat/prp-paint-bucket_Modeling_v0006_bucket_mat_BaseColor_ACES - ACEScg.1001.png"),
+    "prp_paint_bucket_Rigging2:prp_paint_bucket_": os.path.join(
+        base_dir,
+        "Props/prp-paint-bucket/Export/Texturing/v001/handle_mat/prp-paint-bucket_Modeling_v0006_handle_mat_BaseColor_ACES - ACEScg.1001.png"),
+    "prp_paint_bucket_Rigging:prp_paint_bucket_Model": os.path.join(
+        base_dir,
+        "Props/prp-paint-bucket/Export/Texturing/v001/bucket_mat/prp-paint-bucket_Modeling_v0006_bucket_mat_BaseColor_ACES - ACEScg.1001.png"),
+    "prp_paint_bucket_Rigging:prp_paint_bucket_M": os.path.join(
+        base_dir,
+        "Props/prp-paint-bucket/Export/Texturing/v001/handle_mat/prp-paint-bucket_Modeling_v0006_handle_mat_BaseColor_ACES - ACEScg.1001.png"),
+    "prp_paint_bucket_Rigging1rp_paint_bucket_Mode": os.path.join(
+        base_dir,
+        "Props/prp-paint-bucket/Export/Texturing/v001/bucket_mat/prp-paint-bucket_Modeling_v0006_bucket_mat_BaseColor_ACES - ACEScg.1001.png"),
+    "prp_paint_bucket_Rigging1:prp_paint_bucket_": os.path.join(
+        base_dir,
+        "Props/prp-paint-bucket/Export/Texturing/v001/handle_mat/prp-paint-bucket_Modeling_v0006_handle_mat_BaseColor_ACES - ACEScg.1001.png"),
+
+    # ------------------------------------------------------------------- paint bucket 2
+    "prp_paint_bucket2_Rigging:prp_paint_bucket2_Mod": os.path.join(
+        base_dir,
+        "Props/prp-paint-bucket2/Export/Texturing/v001/bucket_mat/prp-paint-bucket2_Modeling_v0006_bucket_mat_BaseColor_ACES - ACEScg.1001.png"),
+    "prp_paint_bucket2_Rigging:prp_paint_bucket2": os.path.join(
+        base_dir,
+        "Props/prp-paint-bucket2/Export/Texturing/v001/handle_mat/prp-paint-bucket2_Modeling_v0006_handle_mat_BaseColor_ACES - ACEScg.1001.png"),
+    "prp_paint_bucket2_Rigging1:prp_paint_bucket2_Mo": os.path.join(
+        base_dir,
+        "Props/prp-paint-bucket2/Export/Texturing/v001/bucket_mat/prp-paint-bucket2_Modeling_v0006_bucket_mat_BaseColor_ACES - ACEScg.1001.png"),
+    "prp_paint_bucket2_Rigging1:prp_paint_bucket": os.path.join(
+        base_dir,
+        "Props/prp-paint-bucket2/Export/Texturing/v001/handle_mat/prp-paint-bucket2_Modeling_v0006_handle_mat_BaseColor_ACES - ACEScg.1001.png"),
+    "prp_paint_bucket2_Rigging2:prp_paint_bucket2_Mo": os.path.join(
+        base_dir,
+        "Props/prp-paint-bucket2/Export/Texturing/v001/bucket_mat/prp-paint-bucket2_Modeling_v0006_bucket_mat_BaseColor_ACES - ACEScg.1001.png"),
+    "prp_paint_bucket2_Rigging2:prp_paint_bucket": os.path.join(
+        base_dir,
+        "Props/prp-paint-bucket2/Export/Texturing/v001/handle_mat/prp-paint-bucket2_Modeling_v0006_handle_mat_BaseColor_ACES - ACEScg.1001.png"),
+    "prp_paint_bucket2_Rigging3:prp_paint_bucket2_Mo": os.path.join(
+        base_dir,
+        "Props/prp-paint-bucket2/Export/Texturing/v001/bucket_mat/prp-paint-bucket2_Modeling_v0006_bucket_mat_BaseColor_ACES - ACEScg.1001.png"),
+    "prp_paint_bucket2_Rigging3:prp_paint_bucket": os.path.join(
+        base_dir,
+        "Props/prp-paint-bucket2/Export/Texturing/v001/handle_mat/prp-paint-bucket2_Modeling_v0006_handle_mat_BaseColor_ACES - ACEScg.1001.png"),
+
+
+    # ------------------------------------------------------------------- wickie helmet
+    "prp_wickie_helmet_Rigging:prp_wickie_helmet.002":os.path.join(
+            base_dir,
+            "Props/prp-wickie-helmet/Export/Texturing/v001/helmet_mat/prp-wickie-helmet_Modeling_v0004_helmet_mat_BaseColor_ACES - ACEScg.1001.png"),
+    "prp_wickie_helmet_Rigging:prp_wickie_helmet.001":os.path.join(
+            base_dir,
+            "Props/prp-wickie-helmet/Export/Texturing/v001/bubble_mat/prp-wickie-helmet_Modeling_v0004_bubble_mat_BaseColor_ACES - ACEScg.1001.png"),
+    "prp_wickie_helmet_Rigging:prp_wickie_helmet_Mod":os.path.join(
+            base_dir,
+            "Props/prp-wickie-helmet/Export/Texturing/v001/bubble_mat/prp-wickie-helmet_Modeling_v0004_bubble_mat_BaseColor_ACES - ACEScg.1001.png"),
+    "prp_wickie_helmet_Rigging:prp_wickie_helmet":os.path.join(
+            base_dir,
+            "Props/prp-wickie-helmet/Export/Texturing/v001/bubble_mat/prp-wickie-helmet_Modeling_v0004_bubble_mat_BaseColor_ACES - ACEScg.1001.png"),
     # tintoy
     #"set_room_Layout:tintoy1":os.path.join(
     #        "M:/frogging_hell_prism/06_Artist/random/2DAssets/02_export/TinToy.png"),
     # canvas 2
-    "set_room_Layout:prp_canvas_02_Modeling:canvas":os.path.join(
+    "prp_canvas_02_Modeling:canvas":os.path.join(
             base_dir,
             "Props/prp-canvas-02/Export/Texturing/v001/projection1-canvas.png"),
     # canvas 3
-    "set_room_Layout:prp_canvas_03_Modeling:prp_canvas":os.path.join(
+    "prp_canvas_03_Modeling:prp_canvas":os.path.join(
             base_dir,
-            "Props/prp-canvas-03/Export/Texturing/v001/projection1-prp_canvas.png")
-    }
+            "Props/prp-canvas-03/Export/Texturing/v001/projection1-prp_canvas.png"),
+    # broom
+    "prp_broom_big_Rigging:prp_broom_big_Modeling:pC":os.path.join(
+            base_dir,
+            "Props/prp-broom-big/Export/Texturing/v001/prp-broom-big_Modeling_v0004_DefaultMaterial_BaseColor_ACES - ACEScg.1001.png"),
+    # drawer small drawer
+    "prp_drawer_smol_Rigging:prp_drawer_smol_Mod.002":os.path.join(
+            base_dir,
+            "Props/prp-drawer-smol/Export/Texturing/v001/drawer_mat/prp-drawer-smol_Modeling_v0020_drawer_mat_BaseColor_ACES - ACEScg.1001.png"),
+    "prp_drawer_smol_Rigging:prp_drawer_smol_Mod":os.path.join(
+            base_dir,
+            "Props/prp-drawer-smol/Export/Texturing/v001/drawer_mat/prp-drawer-smol_Modeling_v0020_drawer_mat_BaseColor_ACES - ACEScg.1001.png"),
+    # drawer small body
+    "prp_drawer_smol_Rigging:prp_drawer_smol_Mod.005":os.path.join(
+            base_dir,
+            "Props/prp-drawer-smol/Export/Texturing/v001/body_mat/prp-drawer-smol_Modeling_v0020_body_mat_BaseColor_ACES - ACEScg.1001.png"),
+    # drawer knobs
+    "prp_drawer_smol_Rigging:prp_drawer_smol_Mod.007":os.path.join(
+            base_dir,
+            "Props/prp-drawer-smol/Export/Texturing/v001/knob_mat/prp-drawer-smol_Modeling_v0020_knob_mat_BaseColor_ACES - ACEScg.1001.png"),
+    "prp_drawer_smol_Rigging:prp_drawer_smol_Mod.008":os.path.join(
+            base_dir,
+            "Props/prp-drawer-smol/Export/Texturing/v001/knob_mat/prp-drawer-smol_Modeling_v0020_knob_mat_BaseColor_ACES - ACEScg.1001.png"),
+    "prp_drawer_smol_Rigging:prp_drawer_smol_Mod.009":os.path.join(
+            base_dir,
+            "Props/prp-drawer-smol/Export/Texturing/v001/knob_mat/prp-drawer-smol_Modeling_v0020_knob_mat_BaseColor_ACES - ACEScg.1001.png"),
+    "prp_drawer_smol_Rigging:prp_drawer_smol_Mod.010":os.path.join(
+            base_dir,
+            "Props/prp-drawer-smol/Export/Texturing/v001/knob_mat/prp-drawer-smol_Modeling_v0020_knob_mat_BaseColor_ACES - ACEScg.1001.png"),
+    # spatula
+    "prp_spongebob_spatula_Rigging:prp_spongebob_spa":os.path.join(
+            base_dir,
+            "Props/prp-spongebob-spatula/Export/Texturing/v001/prp-spongebob-spatula_Modeling_v0004_DefaultMaterial_BaseColor_ACES - ACEScg.1001.png")
+    # ------------------------------------------------------- bucket
+}
 
