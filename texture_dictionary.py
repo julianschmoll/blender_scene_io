9688cccc043f7a342541611f92477674091cda4e
PhongShaderFrog <robmuc@gmx.de>
2024-06-04 15:25:25 +0200

    added object to cryptolists

--- texture_dictionary.py
+++ texture_dictionary.py
@@ -45,20 +45,47 @@ matte_list = ["door_plane",
               "pPlane1"
               ]
 
-frodo_list = ["eye_l",
-              "eye_r",
-              "frog_martins",
-              "frog_whole_pants",
-              "grab_grab",
-              "leggy_boy",
-              "toxic_body_standards"]
+frodo_list = ["chr_frodo_Rigging:eye_l",
+              "chr_frodo_Rigging:eye_r",
+              "chr_frodo_Rigging:chr_frodo_Modeling:frog_martins",
+              "chr_frodo_Rigging:chr_frodo_Modeling:frog_whole_pants",
+              "chr_frodo_Rigging:chr_frodo_Modeling:grab_grab",
+              "chr_frodo_Rigging:chr_frodo_Modeling:leggy_boy",
+              "chr_frodo_Rigging:chr_frodo_Modeling:toxic_body_standards",
+              "chr_frodo_Rigging:eye_l_upper_lid",
+              "chr_frodo_Rigging:eye_l_lower_lid",
+              "chr_frodo_Rigging:chr_optic_nerve_Modeling:opticnerve_r",
+              "chr_frodo_Rigging:eye_r_upper_lid",
+              "chr_frodo_Rigging:eye_r_lower_lid",
+              "chr_frodo_Rigging:chr_optic_nerve_Modeling:optivnerve_l",
+              "chr_frodo_Rigging:chr_teeth_Modeling:top_gums",
+              "chr_frodo_Rigging:chr_teeth_Modeling:bottom_gums",
+              "chr_frodo_Rigging:chr_teeth_Modeling:tooth_14",
+              "chr_frodo_Rigging:chr_teeth_Modeling:tooth_13",
+              "chr_frodo_Rigging:chr_teeth_Modeling:tooth_12",
+              "chr_frodo_Rigging:chr_teeth_Modeling:tooth_11",
+              "chr_frodo_Rigging:chr_teeth_Modeling:tooth_21",
+              "chr_frodo_Rigging:chr_teeth_Modeling:tooth_22",
+              "chr_frodo_Rigging:chr_teeth_Modeling:tooth_23",
+              "chr_frodo_Rigging:chr_teeth_Modeling:tooth_24",
+              "chr_frodo_Rigging:chr_teeth_Modeling:tooth_31",
+              "chr_frodo_Rigging:chr_teeth_Modeling:tooth_32",
+              "chr_frodo_Rigging:chr_teeth_Modeling:tooth_33",
+              "chr_frodo_Rigging:chr_teeth_Modeling:tooth_34",
+              "chr_frodo_Rigging:chr_teeth_Modeling:tooth_41",
+              "chr_frodo_Rigging:chr_teeth_Modeling:tooth_42",
+              "chr_frodo_Rigging:chr_teeth_Modeling:tooth_43",
+              "chr_frodo_Rigging:chr_teeth_Modeling:tooth_44"
+              ]
 
-bed_list = ["prp_mattress",
-            "balls1",
-            "balls2",
-            "balls3",
-            "balls4",
-            "bedframe"
+bed_list = ["prp_bed_Rigging:prp_bed_Modeling:prp_mattress",
+            "prp_bed_Rigging:prp_bed_Modeling:balls1",
+            "prp_bed_Rigging:prp_bed_Modeling:balls2",
+            "prp_bed_Rigging:prp_bed_Modeling:balls3",
+            "prp_bed_Rigging:prp_bed_Modeling:balls4",
+            "prp_bed_Rigging:prp_bed_Modeling:bedframe",
+            "prp_bed_Rigging:blanket_geo",
+            "prp_bed_Rigging:prp_pillow_Modeling:fat_pillow_"
 ]
 
 texture_dict = {
@@ -66,7 +93,7 @@ texture_dict = {
         "door_plane":os.path.join(
             base_dir,
             get_version(matte_path),
-            "main_walls/ projection_walls-door_plane.png"
+            "main_walls/projection_walls-door_plane.png"
         ),
         "floor_plane_right":os.path.join(
             base_dir,
