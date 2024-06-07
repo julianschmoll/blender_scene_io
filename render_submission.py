aca59b8dcb8429afa8dcec2cc4e588ae5dba754e
js435 <dev@julianschmoll.de>
2024-06-07 21:48:49 +0200

    refactor

--- render_submission.py
+++ render_submission.py
@@ -1,38 +1,39 @@
-import bpy
 import os
 import logging
 import subprocess
-import time
+from blender_scene_io import scene_utils
 
 logging.basicConfig(level=logging.INFO)
 LOGGER = logging.getLogger("Render Submission")
 
 
 def submit_render(dry_run=False):
-    scene_path = get_scene_file_path()
-    path_elem = scene_path.split(os.sep)
-    path_elem[0] = "M:\\"
-    naming_elem = path_elem[-1].split("_")
-    nice_name = "_".join(
-        ["Frogging-Hell", path_elem[4], naming_elem[-3], naming_elem[-2]]
-    )
-    parent_path = os.path.dirname(scene_path)
+    scene_path = scene_utils.get_scene_file_path()
     cmd = assemble_cmd(
-        nice_name,
-        create_import_set(parent_path),
+        assemble_render_set_name(scene_path),
+        create_import_set(scene_path),
         scene_path
     )
 
-    set_render_paths(path_elem, naming_elem)
+    scene_utils.set_render_paths()
 
     LOGGER.info(f"Submitting to Renderpal with: \n{cmd}")
 
     if dry_run:
         return
 
-    save_scenefile()
+    scene_utils.save_scenefile()
     run_wake_up_bats()
     subprocess.Popen(cmd)
+
+
+def assemble_render_set_name(scene_path):
+    path_elem = scene_path.split(os.sep)
+    naming_elem = path_elem[-1].split("_")
+    nice_name = "_".join(
+        ["Frogging-Hell", path_elem[4], naming_elem[-3], naming_elem[-2]]
+    )
+    return nice_name
 
 
 def assemble_cmd(render_name, import_set, scene_path, chunk_size=15):
@@ -50,7 +51,8 @@ def assemble_cmd(render_name, import_set, scene_path, chunk_size=15):
     )
 
 
-def create_import_set(parent_path):
+def create_import_set(scene_path):
+    parent_path = os.path.dirname(scene_path)
     content = """
     <RenderSet>
         <Values>
@@ -59,22 +61,13 @@ def create_import_set(parent_path):
             </frames>
         </Values>
     </RenderSet>
-    """.format(*get_frame_ramge())
+    """.format(*scene_utils.get_frame_ramge())
     r_set_file = os.path.join(parent_path, "renderpal.rset")
 
     with open(r_set_file, "w") as r_set:
         r_set.write(content)
 
     return r_set_file
-
-
-def get_scene_file_path():
-    return bpy.data.filepath
-
-
-def get_frame_ramge():
-    scn = bpy.context.scene
-    return scn.frame_start, scn.frame_end
 
 
 def run_wake_up_bats():
@@ -85,26 +78,3 @@ def run_wake_up_bats():
 
 def get_renderpal_exe():
     return "C:\Program Files (x86)\RenderPal V2\CmdRC\RpRcCmd.exe"
-
-
-def set_render_paths(path_elem, naming_elem):
-    LOGGER.info("Setting Render Paths")
-    base_render_path = os.path.join(
-        *path_elem[0:5],
-        "Rendering",
-        "3dRender",
-        naming_elem[4],
-    )
-    file_name = "shot_{0}_3d_####.exr".format(naming_elem[1])
-
-    bpy.context.scene.render.filepath = os.path.join(
-        base_render_path, "rawr", file_name
-    )
-    bpy.context.scene.node_tree.nodes["File Output"].base_path = os.path.join(
-        base_render_path, "out", file_name
-    )
-
-
-def save_scenefile():
-    LOGGER.info("Saving Scenefile")
-    bpy.ops.wm.save_mainfile()
