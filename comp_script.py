7e7553d2620a2fd5b04cc5aa36e2bb9832478dff
js435 <dev@julianschmoll.de>
2024-06-11 12:20:27 +0200

    refactor

--- comp_script.py
+++ comp_script.py
@@ -1,13 +1,8 @@
-from blender_scene_io import texture_dictionary
 import bpy
 import logging
 
-logging.basicConfig(level=logging.INFO)
-LOGGER = logging.getLogger("blender_scene_io")
 
-# -------------------------------------------------------------------------------- COMP ---------------------------------------------------------------------
-def comp_setup():
-    basic_comp_setup()
+LOGGER = logging.getLogger("Comp Setup")
 
 
 def pick_cryptomatte_materials(cryptomatte_node, collection):
@@ -18,36 +13,46 @@ def pick_cryptomatte_materials(cryptomatte_node, collection):
     cryptomatte_node.matte_id = ",".join(material_names)
 
 
-def basic_comp_setup():
-    """
-    This function clears the default compositing nodes, enables nodes and createes our node setup
-    :return:
-    """
-    # use nodes and clear default scene
+def comp_setup():
+    nodetree = prepare_nodetree()
+    master_layer = nodetree.nodes.new("CompositorNodeRLayers")
+    output_node = nodetree.nodes.new("CompositorNodeOutputFile")
+
+    output_node.layer_slots.new("rgb")
+    output_node.layer_slots.new("alpha")
+    nodetree.links.new(master_layer.outputs["Image"],output_node.inputs["Image"])
+    nodetree.links.new(master_layer.outputs["Image"], output_node.inputs["rgb"])
+    nodetree.links.new(master_layer.outputs["Alpha"], output_node.inputs["alpha"])
+
+    setup_render_layers(nodetree, output_node)
+    setup_crypto_mattes(master_layer, nodetree, output_node)
+
+
+def prepare_nodetree():
     bpy.context.scene.use_nodes = True
     nodetree = bpy.context.scene.node_tree
     nodetree.links.clear()
     nodetree.nodes.clear()
-    # add render layer node
-    render_layer_node = nodetree.nodes.new("CompositorNodeRLayers")
-    # add file output node
-    output_node = nodetree.nodes.new("CompositorNodeOutputFile")
-    # add viewer node
-    viewer_node = nodetree.nodes.new("CompositorNodeViewer")
-    # add slots ro node
-    LOGGER.info(output_node.name)
-    output_node.location = (1000,100)
-    viewer_node.location = (1000, 400)
-    render_layer_node.location = (0,200)
-    # ---------------------------------------------------------------------------- connecting nodes
-    # render layer to output
-    nodetree.links.new(render_layer_node.outputs["Image"],output_node.inputs["Image"])
+    return nodetree
+
+
+def setup_crypto_mattes(master_layer, nodetree, output_node):
     for coll in bpy.data.collections:
         if coll.name.lower() not in ["static", "mattes", "cami"]:
-            bed_cryptomatte_node = nodetree.nodes.new("CompositorNodeCryptomatteV2")
-            bed_cryptomatte_node.location = (400, -200)
+            crypto_matte = nodetree.nodes.new("CompositorNodeCryptomatteV2")
             output_node.layer_slots.new(f"{coll.name}_crypto")
-            nodetree.links.new(render_layer_node.outputs["Image"], bed_cryptomatte_node.inputs["Image"])
-            nodetree.links.new(bed_cryptomatte_node.outputs["Image"], output_node.inputs[f"{coll.name}_crypto"])
-            bed_cryptomatte_node.layer_name = 'MasterLayer.CryptoObject'
-            pick_cryptomatte_materials(bed_cryptomatte_node, coll.name)
+            nodetree.links.new(master_layer.outputs["Image"], crypto_matte.inputs["Image"])
+            nodetree.links.new(crypto_matte.outputs["Image"], output_node.inputs[f"{coll.name}_crypto"])
+            crypto_matte.layer_name = "MasterLayer.CryptoObject"
+            pick_cryptomatte_materials(crypto_matte, coll.name)
+
+
+def setup_render_layers(nodetree, output_node):
+    for scene in bpy.data.scenes:
+        for view_layer in scene.view_layers:
+            if view_layer.name == "MasterLayer":
+                continue
+            render_layer = nodetree.nodes.new("CompositorNodeRLayers")
+            render_layer.layer = view_layer.name
+            output_node.layer_slots.new(view_layer.name)
+            nodetree.links.new(render_layer.outputs["Image"], output_node.inputs[view_layer.name])
