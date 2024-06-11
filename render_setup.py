67a66dcf8d8828c0a3784407007fc01869631ab1
js435 <dev@julianschmoll.de>
2024-06-11 12:21:13 +0200

    simplify

--- render_setup.py
+++ render_setup.py
@@ -1,26 +1,21 @@
 import bpy
-import os
 import logging
 
-logging.basicConfig(level=logging.INFO)
-LOGGER = logging.getLogger("blender_scene_io")
+
+LOGGER = logging.getLogger("Render Setup")
+scene = bpy.context.scene
+
 
 def set_render_settings():
+    scene.render.resolution_x = 2048
+    scene.render.resolution_y = 1536
+    scene.render.engine = 'BLENDER_EEVEE'
+    scene.render.fps = 25
+    scene.frame_start = 1001
+    scene.camera.data.clip_end = 100000
 
-    LOGGER.info("Render Script...")
+    scene.render.image_settings.file_format = 'OPEN_EXR_MULTILAYER'
+    scene.render.image_settings.exr_codec = 'ZIP'
 
-    # ------------------------------------------ render settings
-    bpy.context.scene.render.resolution_x = 2048
-    bpy.context.scene.render.resolution_y = 1536
-    bpy.context.scene.render.engine = 'BLENDER_EEVEE'
-    bpy.context.scene.render.fps = 25
-    bpy.context.scene.frame_start = 1001
-    bpy.context.scene.camera.data.clip_end = 100000
-
-    # ------------------------------------------ output settings
-    bpy.context.scene.render.image_settings.file_format = 'OPEN_EXR_MULTILAYER'
-    bpy.context.scene.render.image_settings.exr_codec = 'ZIP'
-
-    # ------------------------------------------ render layers
-    bpy.context.scene.view_layers["ViewLayer"].use_pass_cryptomatte_object = True
-    bpy.context.scene.view_layers["ViewLayer"].use_pass_cryptomatte_material = True
+    scene.view_layers["ViewLayer"].use_pass_cryptomatte_object = True
+    scene.view_layers["ViewLayer"].use_pass_cryptomatte_material = True
