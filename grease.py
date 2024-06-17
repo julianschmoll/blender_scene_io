b5272eb670f3e5e2acee30783c41ac477bca87a1
js435 <dev@julianschmoll.de>
2024-06-17 16:56:13 +0200

    make lines look more like brushes

--- grease.py
+++ grease.py
@@ -7,8 +7,13 @@ LOGGER = logging.getLogger("Grease Utils")
 
 
 def apply_grease_pencil(collection):
+    pixel_size = 300.0
+    if "bed" in collection.name:
+        pixel_size=1000
     gp_mat = add_material(
-        collection, load_image(r"M:\frogging_hell_prism\06_Artist\juschli\brushes\frodo_outline_v3.png")
+        collection,
+        load_image(r"M:\frogging_hell_prism\06_Artist\juschli\brushes\frodo_outline_v7.png"),
+        pixel_size=pixel_size
     )
     gpencil_data = bpy.data.grease_pencils.new(name=collection.name)
     gpencil_object = bpy.data.objects.new(
@@ -78,7 +83,6 @@ def apply_grease_pencil(collection):
         seed = 0,
     )
 
-
     if "frodo" not in collection.name:
         add_gp_modifier(
             gpencil_object,
@@ -99,6 +103,20 @@ def apply_grease_pencil(collection):
         level=2
     )
 
+    add_gp_modifier(
+        gpencil_object,
+        f"{collection.name}_gp_pencil_noise",
+        "GP_NOISE",
+        factor = 0,
+        factor_strength = 0.2,
+        factor_thickness = 0.253521,
+        factor_uvs = 0,
+        noise_scale = 1,
+        noise_offset = 0,
+        seed = 1,
+        use_random = True,
+    )
+
     bpy.data.collections.new(f"{collection.name}_grease")
     bpy.context.scene.collection.children.link(bpy.data.collections[f"{collection.name}_grease"])
     bpy.data.collections[f"{collection.name}_grease"].objects.link(gpencil_object)
@@ -113,7 +131,7 @@ def load_image(path):
     return bpy.data.images.load(path)
 
 
-def add_material(collection, image=None):
+def add_material(collection, image=None, pixel_size=300.0):
     mat_name =f"{collection.name}_gp_material"
     if mat_name in bpy.data.materials.keys():
         gp_mat = bpy.data.materials[mat_name]
@@ -128,7 +146,7 @@ def add_material(collection, image=None):
 
     if image:
         material.stroke_style = "TEXTURE"
-        material.pixel_size = 2000
+        material.pixel_size = pixel_size
         material.use_overlap_strokes = True
         material.stroke_image = image
         material.mix_stroke_factor = 1
