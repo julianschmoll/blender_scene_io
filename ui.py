9319b3b89982fd9df53220b038b1eafde48da60b
js435 <dev@julianschmoll.de>
2024-06-26 15:01:11 +0200

    don't show message box if no ui is available

--- ui.py
+++ ui.py
@@ -1,7 +1,11 @@
 import bpy
+import logging
 
 def ShowMessageBox(message = "", title = "Message Box", icon = 'INFO'):
-
+    if bpy.app.background:
+        logger = logging.getLogger(title)
+        logger.info(message)
+        return
     def draw(self, context):
         self.layout.label(text=message)
 
